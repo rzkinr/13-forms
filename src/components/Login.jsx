46198a45ea566de72ee4262543b3e3398b078101
rzkinr <rizkynurrokhim18@gmail.com>
2025-10-15 10:39:12 +0700

    validate

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,8 @@
-import {useRef} from 'react';
+import {useRef, useState} from 'react';
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const email = useRef();
   const password = useRef();
 
@@ -10,14 +12,25 @@ export default function Login() {
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
 
-    console.log({
-      email: enteredEmail,
-      password: enteredPassword,
-    });
+    // console.log({
+    //   email: enteredEmail,
+    //   password: enteredPassword,
+    // });
 
     // not recommended to manipulate DOM directly
     // email.current.value = '';
     // password.current.value = '';
+
+    const emailIsValid = enteredEmail.includes('@');
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
+
+    console.log('sending http request...');
   }
 
   return (
@@ -28,6 +41,9 @@ export default function Login() {
         <div className='control no-margin'>
           <label htmlFor='email'>Email</label>
           <input id='email' type='email' name='email' ref={email} />
+          <div className='control-error'>
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className='control no-margin'>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,4 +1,8 @@
+import {useState} from 'react';
+
 export default function Signup() {
+  const [passwordAreNotEqual, setPasswordAreNotEqual] = useState(false);
+
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -6,6 +10,12 @@ export default function Signup() {
     const acquisitionChannel = formData.getAll('acquisition');
     const data = Object.fromEntries(formData);
     data.acquisition = acquisitionChannel;
+
+    if (data.password !== data['confirm-password']) {
+      setPasswordAreNotEqual(true);
+      return;
+    }
+
     console.log(data);
   }
   return (
@@ -15,13 +25,19 @@ export default function Signup() {
 
       <div className='control'>
         <label htmlFor='email'>Email</label>
-        <input id='email' type='email' name='email' />
+        <input id='email' type='email' name='email' required />
       </div>
 
       <div className='control-row'>
         <div className='control'>
           <label htmlFor='password'>Password</label>
-          <input id='password' type='password' name='password' />
+          <input
+            id='password'
+            type='password'
+            name='password'
+            required
+            minLength={6}
+          />
         </div>
 
         <div className='control'>
@@ -30,7 +46,12 @@ export default function Signup() {
             id='confirm-password'
             type='password'
             name='confirm-password'
+            required
+            minLength={6}
           />
+          <div className='control-error'>
+            {passwordAreNotEqual && 'Password not equal'}
+          </div>
         </div>
       </div>
 
@@ -39,18 +60,18 @@ export default function Signup() {
       <div className='control-row'>
         <div className='control'>
           <label htmlFor='first-name'>First Name</label>
-          <input type='text' id='first-name' name='first-name' />
+          <input type='text' id='first-name' name='first-name' required />
         </div>
 
         <div className='control'>
           <label htmlFor='last-name'>Last Name</label>
-          <input type='text' id='last-name' name='last-name' />
+          <input type='text' id='last-name' name='last-name' required />
         </div>
       </div>
 
       <div className='control'>
         <label htmlFor='phone'>What best describes your role?</label>
-        <select id='role' name='role'>
+        <select id='role' name='role' required>
           <option value='student'>Student</option>
           <option value='teacher'>Teacher</option>
           <option value='employee'>Employee</option>
@@ -89,8 +110,13 @@ export default function Signup() {
 
       <div className='control'>
         <label htmlFor='terms-and-conditions'>
-          <input type='checkbox' id='terms-and-conditions' name='terms' />I
-          agree to the terms and conditions
+          <input
+            type='checkbox'
+            id='terms-and-conditions'
+            name='terms'
+            required
+          />
+          I agree to the terms and conditions
         </label>
       </div>
 
