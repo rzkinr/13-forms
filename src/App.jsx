b3f5ab1ebfa1668b049b87c5aa105055b52b5ddd
rzkinr <rizkynurrokhim18@gmail.com>
2025-10-15 15:03:13 +0700

    useactionstate

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header.jsx';
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
 import StateLogin from './components/StateLogin.jsx';
+import SignupFormAction from './components/SignupFormAction.jsx';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <main>
         {/* <Login /> */}
         {/* <Signup /> */}
-        <StateLogin />
+        {/* <StateLogin /> */}
+        <SignupFormAction />
       </main>
     </>
   );
---
+++ src/components/SignupFormAction.jsx
@@ -0,0 +1,216 @@
+import {useActionState} from 'react';
+import {isEmail, isNotEmpty, isEqualsToOtherValue} from '../util/validation.js';
+
+export default function SignupFormAction() {
+  function signupAction(prevFormState, formData) {
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const confirmPassword = formData.get('confirm-password');
+    const firstName = formData.get('first-name');
+    const lastName = formData.get('last-name');
+    const role = formData.get('role');
+    const acquisition = formData.getAll('acquisition');
+    const terms = formData.get('terms');
+
+    let errors = [];
+
+    if (!isEmail(email)) {
+      errors.push('Please enter a valid email address.');
+    }
+
+    if (!isNotEmpty(password) || password.length < 6) {
+      errors.push(
+        'Please enter a password that is at least 6 characters long.',
+      );
+    }
+
+    if (!isEqualsToOtherValue(password, confirmPassword)) {
+      errors.push('The password confirmation does not match the password.');
+    }
+
+    if (!isNotEmpty(firstName) || !isNotEmpty(lastName)) {
+      errors.push('Please enter your full name.');
+    }
+
+    if (!isNotEmpty(role)) {
+      errors.push('Please select a role that best describes you.');
+    }
+
+    if (!terms) {
+      errors.push('You must agree to the terms and conditions.');
+    }
+
+    if (acquisition.length === 0) {
+      errors.push('Please let us know how you found us.');
+    }
+
+    if (errors.length > 0) {
+      return {
+        errors,
+        enteredValues: {
+          email,
+          password,
+          confirmPassword,
+          firstName,
+          lastName,
+          role,
+          acquisition,
+          terms,
+        },
+      };
+    }
+
+    return {errors: null};
+  }
+
+  const [formState, formAction] = useActionState(signupAction, {errors: null});
+
+  return (
+    <form action={formAction}>
+      <h2>Welcome on board!</h2>
+      <p>We just need a little bit of data from you to get you started 🚀</p>
+
+      <div className='control'>
+        <label htmlFor='email'>Email</label>
+        <input
+          id='email'
+          type='email'
+          name='email'
+          defaultValue={formState.enteredValues?.email}
+        />
+      </div>
+
+      <div className='control-row'>
+        <div className='control'>
+          <label htmlFor='password'>Password</label>
+          <input
+            id='password'
+            type='password'
+            name='password'
+            defaultValue={formState.enteredValues?.password}
+          />
+        </div>
+
+        <div className='control'>
+          <label htmlFor='confirm-password'>Confirm Password</label>
+          <input
+            id='confirm-password'
+            type='password'
+            name='confirm-password'
+            defaultValue={formState.enteredValues?.confirmPassword}
+          />
+        </div>
+      </div>
+
+      <hr />
+
+      <div className='control-row'>
+        <div className='control'>
+          <label htmlFor='first-name'>First Name</label>
+          <input
+            type='text'
+            id='first-name'
+            name='first-name'
+            defaultValue={formState.enteredValues?.firstName}
+          />
+        </div>
+
+        <div className='control'>
+          <label htmlFor='last-name'>Last Name</label>
+          <input
+            type='text'
+            id='last-name'
+            name='last-name'
+            defaultValue={formState.enteredValues?.lastName}
+          />
+        </div>
+      </div>
+
+      <div className='control'>
+        <label htmlFor='phone'>What best describes your role?</label>
+        <select
+          id='role'
+          name='role'
+          defaultValue={formState.enteredValues?.role}>
+          <option value='student'>Student</option>
+          <option value='teacher'>Teacher</option>
+          <option value='employee'>Employee</option>
+          <option value='founder'>Founder</option>
+          <option value='other'>Other</option>
+        </select>
+      </div>
+
+      <fieldset>
+        <legend>How did you find us?</legend>
+        <div className='control'>
+          <input
+            type='checkbox'
+            id='google'
+            name='acquisition'
+            value='google'
+            defaultChecked={formState.enteredValues?.acquisition?.includes(
+              'google',
+            )}
+          />
+          <label htmlFor='google'>Google</label>
+        </div>
+
+        <div className='control'>
+          <input
+            type='checkbox'
+            id='friend'
+            name='acquisition'
+            value='friend'
+            defaultChecked={formState.enteredValues?.acquisition?.includes(
+              'friend',
+            )}
+          />
+          <label htmlFor='friend'>Referred by friend</label>
+        </div>
+
+        <div className='control'>
+          <input
+            type='checkbox'
+            id='other'
+            name='acquisition'
+            value='other'
+            defaultChecked={formState.enteredValues?.acquisition?.includes(
+              'other',
+            )}
+          />
+          <label htmlFor='other'>Other</label>
+        </div>
+      </fieldset>
+
+      <div className='control'>
+        <label htmlFor='terms-and-conditions'>
+          <input
+            type='checkbox'
+            id='terms-and-conditions'
+            name='terms'
+            defaultChecked={formState.enteredValues?.terms}
+          />
+          I agree to the terms and conditions
+        </label>
+      </div>
+
+      {formState.errors && (
+        <div className='error'>
+          <p>The following errors occurred:</p>
+          <ul>
+            {formState.errors.map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      <p className='form-actions'>
+        <button type='reset' className='button button-flat'>
+          Reset
+        </button>
+        <button className='button'>Sign up</button>
+      </p>
+    </form>
+  );
+}
